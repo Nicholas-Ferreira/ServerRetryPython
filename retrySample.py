1567bffb4a05ff851ab8b7b3bb4a516567de0c52
Nicholas Ferreira <nichoas_sc@live.com>
2020-10-22 23:09:22 -0300

    removendo params nao usado

--- retrySample.py
+++ retrySample.py
@@ -4,9 +4,9 @@ from retry.api import retry_call
 
 logging_logger = logging.getLogger(__name__)
 
-def request_api(service, info=None):
+def request_api(service):
   r = requests.get(service)
-  
+
   if(r.status_code != requests.codes.ok):
     r.raise_for_status()
   return r.text
